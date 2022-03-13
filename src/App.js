d8edb7cdbcb07e8607a83ac1ed8af711351ad1ba
saitgumus <saitgumus33@gmail.com>
2022-03-13 21:16:07 +0300

    tender modülü kaldırılması.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { Route, Switch } from "react-router";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
 import Distributor from "./components/Distributor";
-import DefineSiteApartment from "./components/Admin/SiteApartment/DefineSiteApartment";
-import MemberDefine from "./components/Admin/Member/member-define";
-import MemberList from "./components/Admin/Member/member-list";
-import UserProfile from "./components/User/user-profile";
-import InboxComponent from "./components/User/Inbox";
-import VoteDefining from "./components/Vote/VoteDefining";
-import VotingAndResult from "./components/Vote/VotingAndResult";
-import ExpenseEntry from "./components/Accounting/expense-entry";
-import ExpenseList from "./components/Accounting/expense-list";
-import TenderDefining from "./components/Tender/tender-defining-component";
-import TenderList from "./components/Tender/tender-list-component";
-import DuesPayment from "./components/Accounting/dues-payment";
-import DuesListing from "./components/Accounting/dues-listing";
+// import DefineSiteApartment from "./components/Admin/SiteApartment/DefineSiteApartment";
+// import MemberDefine from "./components/Admin/Member/member-define";
+// import MemberList from "./components/Admin/Member/member-list";
+// import UserProfile from "./components/User/user-profile";
+// import InboxComponent from "./components/User/Inbox";
+// import VoteDefining from "./components/Vote/VoteDefining";
+// import VotingAndResult from "./components/Vote/VotingAndResult";
+// import ExpenseEntry from "./components/Accounting/expense-entry";
+// import ExpenseList from "./components/Accounting/expense-list";
+// import TenderDefining from "./components/Tender/tender-defining-component";
+// import TenderList from "./components/Tender/tender-list-component";
+// import DuesPayment from "./components/Accounting/dues-payment";
+// import DuesListing from "./components/Accounting/dues-listing";
 
 export default class App extends Component {
   static displayName = App.name;
@@ -24,13 +24,13 @@ export default class App extends Component {
     return (
       <Layout>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Home}/>
           <Route exact path="/distributor" component={Distributor} />
-          <Route
+          {/* <Route
             path="/admin/definesiteapartment"
             component={DefineSiteApartment}
-          />
-          <Route path="/admin/definemember" component={MemberDefine} />
+          /> */}
+          {/* <Route path="/admin/definemember" component={MemberDefine} />
           <Route path="/admin/votedefining" component={VoteDefining} />
           <Route path="/admin/memberlist" component={MemberList} />
           <Route path="/user/profile" component={UserProfile} />
@@ -41,7 +41,7 @@ export default class App extends Component {
           <Route path="/tender/tenderdefining" component={TenderDefining} />
           <Route path="/tender/tenderlist" component={TenderList} />
           <Route path="/accounting/dues-payment" component={DuesPayment} />
-          <Route path="/accounting/dues-listing" component={DuesListing} />
+          <Route path="/accounting/dues-listing" component={DuesListing} /> */}
         </Switch>
       </Layout>
     );
